f79e446ebe595bc0e806c90e9159afd4ab07bddf
Rin NoName <noname@MacBook-Pro-Rin.local>
2021-01-20 13:34:28 +0300

    new commit

--- main/viberbot.py
+++ main/viberbot.py
@@ -1,6 +1,10 @@
 import requests
 import json
 
+# wW0D1ooykZdPV0rk7Vv5iw==
+
+# +EdDG1RrsZXcbC3bfUoz/w==
+
 def send_viber_message(message):
     METBOTV_API_TOKEN = '4ca366e7cec00c05-71d303c1932c9df5-409d3fc23db8f30'
     LENA_API_TOKEN = '4cc512f93a67d2c0-1712a832c410d948-471a0d759e4d88ec'
@@ -9,7 +13,7 @@ def send_viber_message(message):
     }
     url = 'https://chatapi.viber.com/pa/send_message'
     data = {
-        "receiver":"+EdDG1RrsZXcbC3bfUoz/w==",
+        "receiver":"wW0D1ooykZdPV0rk7Vv5iw==",
         "min_api_version":1,
         "sender": {
             "name":"lena photo bot"
